28d7474bf722ed8f7f541e5161933b8962261014
hashimoto <lanshimaoli810@gmail.com>
2024-07-10 10:33:24 +0900

    7/10 最初のコミット

---
+++ src/main/java/com/example/demo/BugKnights2BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugKnights2BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugKnights2BackApplication.class, args);
+	}
+
+}
