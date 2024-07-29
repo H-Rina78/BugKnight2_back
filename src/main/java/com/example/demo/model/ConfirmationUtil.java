fe528084d58999bae8e76bbec8f80cc9d76201dd
kaede <kaede.kn@outlook.jp>
2024-07-29 17:21:39 +0900

    komitto

--- src/main/java/com/example/demo/model/ConfirmationUtil.java
+++ src/main/java/com/example/demo/model/ConfirmationUtil.java
@@ -1,21 +1,35 @@
 package com.example.demo.model;
 
-import java.util.Random;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Base64;
 
 public class ConfirmationUtil {
-	public static String codeGenerate(int length) {
-		// 使用可能な文字の定義
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        StringBuilder sb = new StringBuilder(length);
-        Random random = new Random();
-
-        // 指定された長さまでランダムな文字列を生成
-        for (int i = 0; i < length; i++) {
-            // characters文字列からランダムに1文字選ぶ
-            int index = random.nextInt(characters.length());
-            sb.append(characters.charAt(index));
+	
+	public static String encodeSessionId(String sessionId) {
+        String encodeSessionId = "";
+        try {
+        	System.out.println("sessionId" + sessionId);
+        	String encodedString = Base64.getEncoder().encodeToString(sessionId.getBytes());
+        	System.out.println("sessionId" + sessionId);
+            encodeSessionId = URLEncoder.encode(encodedString, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
-
-        return sb.toString();
-	}
+        return encodeSessionId;
+    }
+	
+	public static String decodeSessionId(String sessionId) {
+        String decodeSessionId = "";
+        try {
+            String decodedString = URLDecoder.decode(sessionId, "UTF-8");
+            System.out.println(decodedString);
+            decodeSessionId = new String(Base64.getDecoder().decode(decodedString));
+            System.out.println(decodeSessionId);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return decodeSessionId;
+    }
 }
